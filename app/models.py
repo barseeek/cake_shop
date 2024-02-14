24f05160a64d8d219aae1371b7b68d82772d44fa
Денис Малых <malykhdenis@gmail.com>
2024-02-14 18:43:47 +0300

    add Client model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """Модель клиента."""
+    username = models.CharField('Имя пользователя в Telegram', max_length=100)
+    address = models.TextField('Адрес')
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Клиент'
+        verbose_plural_name = 'Клиенты'
+
+    def __str__(self):
+        return self.username
