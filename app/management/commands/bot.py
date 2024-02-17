07ac45b10f3a0e4d4b5e163e6941ffb4249b249b
Nikita <barskii.no@outlook.com>
2024-02-17 18:27:59 +0300

    Update models, finish states

--- app/management/commands/bot.py
+++ app/management/commands/bot.py
@@ -7,6 +7,7 @@ from telebot.handler_backends import State, StatesGroup
 from telebot.storage import StateMemoryStorage
 from telebot.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, \
     InlineKeyboardButton
+import datetime
 
 env = Env()
 env.read_env()
@@ -27,21 +28,96 @@ class BotStates(StatesGroup):
     select_inscription = State()
     get_inscription = State()
     get_address = State()
+    get_date = State()
+    get_time = State()
+    create_order = State()
+    after_order = State()
 
 
-@bot.message_handler(state=BotStates.get_address, func= lambda message: True)
-def get_inscription(message):
+@bot.message_handler(state=BotStates.after_order, func=lambda message: True)
+def create_order(message):
+    bot.send_message(message.chat.id, "Хотите заказать новый торт? Для нового заказа через бота введите /start")
+    bot.set_state(message.from_user.id, BotStates.after_order, message.chat.id)
+
+
+@bot.message_handler(state=BotStates.create_order, func=lambda message: True)
+def create_order(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data["comment"] = message.text
+    msg = "Отлично, вот данные по вашему заказу: \n"
+    for key, value in data.items():
+        msg += f"{key}:{value}\n"
+    msg += "Ваш заказ №бла создан"
+    bot.send_message(message.chat.id, msg)
+    bot.set_state(message.from_user.id, BotStates.after_order, message.chat.id)
+
+
+@bot.callback_query_handler(state=BotStates.get_time, func=lambda call: True)
+def get_time(call):
+    message = call.message
+    chat_id = message.chat.id
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["time"] = call.data
+    bot.send_message(chat_id, "Введите комментарий для курьера")
+    bot.set_state(chat_id, BotStates.create_order)
+
+
+@bot.callback_query_handler(state=BotStates.get_date, func=lambda call: call.data == "YES" or call.data == "fast_delivery")
+def get_date(call):
+    message = call.message
+    chat_id = message.chat.id
+    delivery_date = datetime.datetime.now() + datetime.timedelta(days=2)
+    if call.data == "fast_delivery":
+        delivery_date = datetime.datetime.now().date()
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["date"] = delivery_date.date()
+    inline_keyboard = InlineKeyboardMarkup(row_width=2)
+    inline_keyboard.add(InlineKeyboardButton("9-13", callback_data="9-13"),
+                        InlineKeyboardButton("13-17", callback_data="13-17"),
+                        InlineKeyboardButton("17-21", callback_data="17-21"))
+    bot.send_message(chat_id, "Выберите временной интервал", reply_markup=inline_keyboard)
+    bot.set_state(chat_id, BotStates.get_time)
+
+
+@bot.callback_query_handler(state=BotStates.get_date, func=lambda call: call.data == "call_manager")
+def call_manager(call):
+    message = call.message
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Менеджер Ирина: +79211233446")
+    bot.set_state(chat_id, BotStates.after_order)
+
+
+@bot.callback_query_handler(state=BotStates.get_date, func=lambda call: call.data == "NO")
+def get_another_date(call):
+    message = call.message
+    chat_id = message.chat.id
+    inline_keyboard = InlineKeyboardMarkup(row_width=2)
+    inline_keyboard.add(InlineKeyboardButton("Срочная доставка", callback_data="fast_delivery"),
+                        InlineKeyboardButton("Позвонить менеджеру", callback_data="call_manager"))
+    bot.send_message(chat_id, "Мы можем предложить срочную доставку в течение 24 часов, это прибавит 20% к стоимости заказа, либо можно позвонить менеджеру", reply_markup=inline_keyboard)
+
+
+@bot.message_handler(state=BotStates.get_address, func=lambda message: True)
+def get_address(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data["address"] = message.text
     inline_keyboard = InlineKeyboardMarkup(row_width=2)
     button_yes = InlineKeyboardButton("Да", callback_data="YES")
     button_no = InlineKeyboardButton("Нет", callback_data="NO")
     inline_keyboard.add(button_yes, button_no)
-    bot.send_message(message.chat.id, "Отлично! Ближайшая дата доставки послезавтра, устроит?")
-    bot.set_state(message.from_user.id, BotStates.get_address, message.chat.id)
+    delivery_date = datetime.datetime.now() + datetime.timedelta(days=2)
+    bot.send_message(message.chat.id, f"Отлично! Ближайшая дата доставки {delivery_date.date()}, устроит?",
+                     reply_markup=inline_keyboard)
+    bot.set_state(message.from_user.id, BotStates.get_date, message.chat.id)
 
-@bot.message_handler(state=BotStates.get_inscription, func= lambda message: True)
+
+@bot.message_handler(state=BotStates.get_inscription, func=lambda message: True)
 def get_inscription(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data["inscription"] = message.text
     bot.send_message(message.chat.id, "Понял вас. Теперь введите адрес")
     bot.set_state(message.from_user.id, BotStates.get_address, message.chat.id)
+
 
 @bot.callback_query_handler(state=BotStates.get_inscription, func=lambda call: True)
 def custom_cake_inscription(call):
@@ -49,9 +125,9 @@ def custom_cake_inscription(call):
     chat_id = message.chat.id
     bot.edit_message_reply_markup(chat_id, message.message_id)
     if call.data == "YES":
-        bot.send_message(chat_id,"Введите надпись на торт")
+        bot.send_message(chat_id, "Введите надпись на торт")
     elif call.data == "NO":
-        bot.send_message(chat_id,"Хорошо. Введите адрес доставки")
+        bot.send_message(chat_id, "Хорошо. Введите адрес доставки")
         bot.set_state(chat_id, BotStates.get_address)
     else:
         print(call.data)
@@ -61,6 +137,8 @@ def custom_cake_inscription(call):
 def custom_cake_inscription(call):
     message = call.message
     chat_id = message.chat.id
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["decor"] = call.data
     bot.edit_message_reply_markup(chat_id, message.message_id)
     inline_keyboard = InlineKeyboardMarkup(row_width=2)
     button_yes = InlineKeyboardButton("Да", callback_data="YES")
@@ -75,6 +153,8 @@ def custom_cake_inscription(call):
 def custom_cake_decor(call):
     message = call.message
     chat_id = message.chat.id
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["berries"] = call.data
     bot.edit_message_reply_markup(chat_id, message.message_id)
     inline_keyboard = InlineKeyboardMarkup(row_width=3)
     inline_keyboard.add(InlineKeyboardButton("Без декора", callback_data="no_decor"),
@@ -93,6 +173,8 @@ def custom_cake_decor(call):
 def custom_cake_berries(call):
     message = call.message
     chat_id = message.chat.id
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["topping"] = call.data
     bot.edit_message_reply_markup(chat_id, message.message_id)
     inline_keyboard = InlineKeyboardMarkup()
     inline_keyboard.add(InlineKeyboardButton("Без ягод", callback_data="no_berries"),
@@ -109,6 +191,8 @@ def custom_cake_berries(call):
 def custom_cake_topping(call):
     message = call.message
     chat_id = message.chat.id
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["form"] = call.data
     bot.edit_message_reply_markup(chat_id, message.message_id)
     inline_keyboard = InlineKeyboardMarkup()
     inline_keyboard.add(InlineKeyboardButton("Без топпинга", callback_data="no_topping"),
@@ -124,6 +208,8 @@ def custom_cake_topping(call):
 def custom_cake_form(call):
     message = call.message
     chat_id = message.chat.id
+    with bot.retrieve_data(call.from_user.id, chat_id) as data:
+        data["level"] = call.data
     bot.edit_message_reply_markup(chat_id, message.message_id)
     inline_keyboard = InlineKeyboardMarkup()
     inline_keyboard.add(InlineKeyboardButton("Круг", callback_data="circle"),
