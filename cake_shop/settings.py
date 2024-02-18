f7449ca2a92b2462bfdf2837bc148f95fdb5f6e4
Nikita <barskii.no@outlook.com>
2024-02-18 19:24:57 +0300

    update requirements

--- cake_shop/settings.py
+++ cake_shop/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["barseeek.pythonanywhere.com", "www.barseeek.pythonanywhere.com"]
 
 
 # Application definition
